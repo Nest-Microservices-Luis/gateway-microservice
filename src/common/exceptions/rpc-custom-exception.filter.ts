89354713a27ed4c82be286bc31e642f29678704e
LuisRivera2016 <luisi.najera@gmail.com>
2026-03-18 21:01:14 -0600

    Add error handling for empty RPC responses in RpcCustomExceptionFilter
    - Enhanced RpcCustomExceptionFilter to return a 500 status code with a specific message when an 'Empty response' error is encountered.
    - Added logging for the exception name to aid in debugging.

--- src/common/exceptions/rpc-custom-exception.filter.ts
+++ src/common/exceptions/rpc-custom-exception.filter.ts
@@ -9,6 +9,15 @@ export class RpcCustomExceptionFilter implements ExceptionFilter<RpcException> {
     const response = ctx.getResponse();
 
     const rpcError = exception.getError();
+    
+    if (rpcError.toString().includes('Empty response')) {
+      return response.status(500).json({
+        statusCode: 500,
+        message: rpcError.toString().substring(0, rpcError.toString().indexOf('(') - 1)
+      });
+    }
+
+    console.log(name);
 
     if (
       typeof rpcError === 'object' && 
